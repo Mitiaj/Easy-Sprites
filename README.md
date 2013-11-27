fc43f77ef64c871af890c94a8cc279f33c5f967a
Paul <paul@devbridge.com>
2013-11-27 20:41:27 +0200

    init setup, added few icons and spriting mixin.

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-scss-spriting
+SCSS-Spriting
 =============
 
 Spriting plugin for SASS/Compass
+
+##Possible @mixin options
+
+ ```scss
+ $icons-map: icons //default
+ $size: true //default
+ $center: false //by default set to false
+ $center-y: false //by default set to false
+ $center-x: false //by default set to false
+ ```
---
+++ config.rb
@@ -0,0 +1,20 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+
+css_dir = "/Content/Styles"
+sass_dir = "scss"
+images_dir = "/Content/Styles/images"
+
+sprite_load_path = "scss/images"
+relative_assets = true
+sprite_engine = :chunky_png
+chunky_png_options = :best_compression
+
+output_style = :expanded
+line_comments = false
+
+on_sprite_generated do |sprite_data|
+  sprite_data.metadata['Caption'] = "This Image is created by Paul G. &copy; 2013"
+end
